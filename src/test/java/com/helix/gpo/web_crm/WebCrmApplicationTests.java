8a12b1eae4542a87be4afece1ca08d64b716064b
helix-gpo <helix.gpo@gmail.com>
2026-08-07 17:19:08 +0200

    Initial commit: Spring Boot 4.1 modulith skeleton

---
+++ src/test/java/com/helix/gpo/web_crm/WebCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helix.gpo.web_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
